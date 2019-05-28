68b5c16f809cb359f1b909cf0ec8dc835eceb3d6
Oliver-Strange <preston.burton@gmail.com>
2019-05-27 23:35:04 -0700

    setup basic react app with dependencies redux react-redux redux-thunk redux-logger axios react-router-dom styled-components

---
+++ food-passport/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import StyledApp from "./styles";
+
+class App extends Component {
+  render() {
+    return (
+      <StyledApp>
+        <h1>hello</h1>
+      </StyledApp>
+    );
+  }
+}
+
+export default App;
---
+++ food-passport/src/styles/StyledApp.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const StyledApp = styled.div`
+  .App {
+    display: flex;
+  }
+
+  /* .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+    height: 40vmin;
+    pointer-events: none;
+  } */
+
+  /* @keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+} */
+`;
+
+export default StyledApp;
